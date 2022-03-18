448172c27df9c5bad0a7d9a2fc8cd030bb62e4e9
abdus121096 <101717631+abdus121096@users.noreply.github.com>
2022-03-18 21:30:35 +0600

    Add files via upload

---
+++ zadan5.py
@@ -0,0 +1,12 @@
+
+while True:
+    try:
+        b = input('reversed: ')
+                
+        print(b)
+        print('good')
+        break
+    except:
+        print('try again')
+        continue
+    
